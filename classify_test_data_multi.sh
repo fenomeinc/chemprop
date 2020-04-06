706dfa325437fc7db9163d69005cdf0d0df349e6
Terran Lane <tlane@fl65inc.com>
2020-04-06 17:19:53 +0000

    Some more checks and expt documentation for classify script.

--- classify_test_data_multi.sh
+++ classify_test_data_multi.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-source configs/base.sh
+source configs/config_gordon_proteases_unified_data.sh
 source ${HOME}/work/fl65/py65/utils/sh_utils.sh
 
-EXPT_DATE=20200330
+EXPT_DATE=20200402
 OUTPUT_BASE_DIR=${OUTPUT_ROOT}/${EXPT_DATE}
 PREDICTIONS_BASE_DIR=${PREDICTIONS_ROOT}/${EXPT_DATE}
 LOGFILE=${OUTPUT_BASE_DIR}/runner_rig.log
+
+mkdir -p "${PREDICTIONS_BASE_DIR}"
+py65::assert_exists "${TEST_SPLIT}"
 
 py65::log "Starting classification."
 py65::log "Input data from ${TEST_SPLIT}"
 py65::log "Log file at ${LOGFILE}"
 py65::log "Predictions written to ${PREDICTIONS_BASE_DIR}"
+
+echo "${EXPT_DESCR}" > "${PREDICTIONS_BASE_DIR}/experiment_description.txt"
 
 for iter in 0 1 2 3 4 5; do
   PREDICTIONS_PATH=${PREDICTIONS_BASE_DIR}/${iter}/fold_0
